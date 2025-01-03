4e0102e46bc22fbab6552bc16d67d3c6080fcd40
Pierre Josselin <70814177+pierre-josselin@users.noreply.github.com>
2025-01-03 18:26:07 +0100

    Update notification-controller.js

--- src/controllers/notification-controller.js
+++ src/controllers/notification-controller.js
@@ -1,9 +1,13 @@
+const dotenv = require("dotenv");
+
 const { triggerEvent } = require("../managers/event-manager.js");
+
+dotenv.config();
 
 async function handleNotification(request, response) {
     const { eid } = request.params;
 
-    const oid = request.body?.data?.[0]?.oid?.value;
+    const oid = request.body?.data?.[0]?.[process.env.CONTEXT_BROKER_ENTITY_PROPERTY_OID]?.value;
     if (!oid) {
         return response.sendStatus(400);
     }
